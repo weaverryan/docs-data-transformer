5b579bae2e16ca20cbe2f1c0c1ad73338b1b8f66
Ryan Weaver <ryan@thatsquality.com>
2015-06-27 20:10:39 -0400

    finishing up, adding edit

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Task;
 use AppBundle\Form\TaskType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -10,19 +11,48 @@ use Symfony\Component\HttpFoundation\Request;
 class TaskController extends Controller
 {
     /**
-     * @Route("/task/new", name="homepage")
+     * @Route("/task/new", name="task_new")
      */
     public function indexAction(Request $request)
     {
-        $form = $this->createForm(new TaskType());
+        $task = new Task();
+        $form = $this->createForm(new TaskType(), $task);
 
         $form->handleRequest($request);
         if ($form->isValid()) {
-            // just dump the Task object
-            dump($form->getData());die;
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirectToRoute('task_edit', [
+                'id' => $task->getId(),
+            ]);
         }
 
         return $this->render('task/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/task/{id}", name="task_edit")
+     */
+    public function editAction(Task $task, Request $request)
+    {
+        $form = $this->createForm(new TaskType(), $task);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirectToRoute('task_edit', [
+                'id' => $task->getId(),
+            ]);
+        }
+
+        return $this->render('task/edit.html.twig', array(
             'form' => $form->createView()
         ));
     }
